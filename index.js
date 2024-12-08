e2e1733ac2b2d998201c608ad3a9ffcec0421e97
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-12-08 18:49:59 +0530

    Store

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const ORDER_CAKE = 'ORDER_CAKE'
+const redux = require('redux');
+const createStore = redux.createStore;
 
-function orderCake(){
+const ORDER_CAKE = 'ORDER_CAKE';
+
+function orderCake() {
   return {
     type: ORDER_CAKE,
     payload: 1
-  }
+  };
 }
 
-initialState = {
+const initialState = {
   numOfCakes: 10
-}
+};
 
-const cakeReducer = (state=initialState, action) => {
-  switch(action.type){
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
     case ORDER_CAKE:
-      return { ...state, numOfCakes: state.numOfCakes - 1 }
-    default: return state
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
+    default:
+      return state;
   }
-} 
+};
+
+const store = createStore(cakeReducer);
+console.log('Initial State', store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log('Updated State', store.getState())
+);
+
+// Invoke the `orderCake` function to get the action object
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
