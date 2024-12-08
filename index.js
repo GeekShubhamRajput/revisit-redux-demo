e338836a5e253fb9213af7ac1b6ba0d05f8025ed
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-12-08 19:09:28 +0530

    Restocking Cake

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const ORDER_CAKE = 'ORDER_CAKE';
+const RESTOCK_CAKE = 'RESTOCK_CAKE';
 
 function orderCake() {
   return {
-    type: ORDER_CAKE,
-    payload: 1
+    type: ORDER_CAKE
+  };
+}
+
+function restockCake(qty=1) {
+  return {
+    type: RESTOCK_CAKE,
+    payload: qty
   };
 }
 
 const initialState = {
-  numOfCakes: 10
+  numOfCakes: 10,
+  payload: 1
 };
+
+
 
 const cakeReducer = (state = initialState, action) => {
   switch (action.type) {
     case ORDER_CAKE:
       return { ...state, numOfCakes: state.numOfCakes - 1 };
+    case RESTOCK_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes + action.payload };
     default:
       return state;
   }
@@ -34,5 +46,6 @@ store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(4));
 
 unsubscribe();
